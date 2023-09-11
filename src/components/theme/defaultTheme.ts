cf5735fcfd56af130f05b7c169429f93daf19a0e
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2023-09-10 23:08:31 -0500

    integrated theming with Storybook and Emotion

---
+++ src/components/theme/defaultTheme.ts
@@ -0,0 +1,20 @@
+const theme = {
+  colors: {
+    primary: '#002D62',
+    accent: '#EB6E1F',
+    white: '#FFFFFF'
+  },
+  typography: {
+    button: {
+      fontFamily: "'Helvetica Neue', 'Sans-serif'",
+      fontWeight: 'bold',
+      fontSize: 32,
+      lineHeight: 56
+    }
+  },
+  units: {
+    px: 'px'
+  }
+}
+
+export default theme
