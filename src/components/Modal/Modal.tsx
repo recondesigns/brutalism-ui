eae39d3aac8011f394927fc61be28789b2ec3656
Michael Stedman <michaelstedman81@gmail.com>
2023-11-08 15:09:25 -0600

    feat: Added ESLint and fixed linting errors

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,29 +1,29 @@
-import React from "react"
-import styled from "@emotion/styled"
-import ModalOverlay from "./ModalOverlay"
-import ModalContainer from "./ModalContainer"
+import React from 'react'
+import styled from '@emotion/styled'
+import ModalOverlay from './ModalOverlay'
+import ModalContainer from './ModalContainer'
 
 export type ModalProps = {
   shouldCloseOnEsc?: boolean
   children: React.ReactElement | React.ReactElement[]
 }
 
-const ModalComponent = styled("div")<ModalProps>({
-  position: "absolute",
-  top: 0,
-  right: 0,
-  bottom: 0,
-  left: 0,
+const ModalComponent = styled('div')<ModalProps>({
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
 })
 
 export default function Modal({ children }: ModalProps) {
-  return (
-    <ModalComponent>
-      <ModalOverlay>
-        <ModalContainer>
-          {children}
-        </ModalContainer>
-      </ModalOverlay>
-    </ModalComponent>
-  )
+    return (
+        <ModalComponent>
+            <ModalOverlay>
+                <ModalContainer>
+                    {children}
+                </ModalContainer>
+            </ModalOverlay>
+        </ModalComponent>
+    )
 }
