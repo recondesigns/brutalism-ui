6e21d928047ed29b8df73566bfe67f9353a12bbb
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-07-02 11:44:18 -0500

    feat(): updated after build

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,47 +1,47 @@
-import { Meta, StoryObj } from "@storybook/react"
-import Button from "./Button"
-import { CloseIcon } from "../../assets"
+import { Meta, StoryObj } from '@storybook/react'
+import Button from './Button'
+import { CloseIcon } from '../../assets'
 
 const meta: Meta<typeof Button> = {
-  title: "Components/Button",
+  title: 'Components/Button',
   component: Button,
 }
 
 export const Default: StoryObj<typeof Button> = {
   render: (args) => <Button {...args} />,
   args: {
-    children: "Button",
-    size: "lg",
+    children: 'Button',
+    size: 'lg',
     isFullWidth: false,
     disabled: false,
-    onClick: () => console.log("Button clicked."),
+    onClick: () => console.log('Button clicked.'),
   },
 }
 
 export const FullWidth: StoryObj<typeof Button> = {
   render: (args) => <Button {...args} />,
   args: {
-    children: "Button",
+    children: 'Button',
     isFullWidth: true,
-    onClick: () => console.log("Button clicked."),
+    onClick: () => console.log('Button clicked.'),
   },
 }
 
 export const LeftIcon: StoryObj<typeof Button> = {
   render: (args) => <Button {...args} />,
   args: {
-    children: "Button",
+    children: 'Button',
     leftIcon: <CloseIcon />,
-    onClick: () => console.log("Button clicked."),
+    onClick: () => console.log('Button clicked.'),
   },
 }
 
 export const Disabled: StoryObj<typeof Button> = {
   render: (args) => <Button {...args} />,
   args: {
-    children: "Button",
+    children: 'Button',
     disabled: true,
-    onClick: () => console.log("Button clicked."),
+    onClick: () => console.log('Button clicked.'),
   },
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Button, ButtonProps } from "./Button"
-export { default as Input, InputProps } from "./Input"
-export { Modal, ModalHeader, ModalContent } from "./Modal"
+export { default as Button, ButtonProps } from './Button'
+export { default as Input, InputProps } from './Input'
+export { Modal, ModalHeader, ModalContent } from './Modal'
