a9925b1d5960df09e77feeaf6d1973a13fe55324
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-07-06 18:41:47 -0500

    docs(Card): updated stories and docs

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import styled from '@emotion/styled'
-import { ThemeProvider } from '@emotion/react'
-import { defaultTheme } from '../emotionTheme'
+import React from "react"
+import styled from "@emotion/styled"
+import { ThemeProvider } from "@emotion/react"
+import { defaultTheme } from "../emotionTheme"
 
 type CardWrapper = {
   isClickable?: boolean
   disabled?: boolean
 } & React.HTMLAttributes<HTMLDivElement>
 
-const CardWrapper = styled('div')<CardWrapper>(
+const CardWrapper = styled("div")<CardWrapper>(
   {
-    width: '100%',
-    background: 'none',
+    width: "100%",
+    background: "none",
   },
   ({ theme, disabled, isClickable }) => ({
-    opacity: !disabled ? 'initial' : '50%',
+    opacity: !disabled ? "initial" : "50%",
     border: `2px solid ${theme.palette.common.border}`,
     borderRadius: theme.shape.borderRadius,
     boxShadow: `${theme.elevation.three} ${theme.elevation.three} 0px 0px ${theme.palette.common.shadow}`,
-    '&:hover': {
+    "&:hover": {
       boxShadow:
         isClickable && !disabled
           ? `${theme.elevation.two} ${theme.elevation.two} 0px 0px ${theme.palette.common.shadow}`
           : `${theme.elevation.three} ${theme.elevation.three} 0px 0px ${theme.palette.common.shadow}`,
-      cursor: disabled ? 'not-allowed' : isClickable ? 'pointer' : 'initial',
+      cursor: disabled ? "not-allowed" : isClickable ? "pointer" : "initial",
     },
-    '&:active': {
+    "&:active": {
       boxShadow:
         isClickable && !disabled
           ? `${theme.elevation.none} ${theme.elevation.none} 0px 0px ${theme.palette.common.shadow}`
@@ -35,14 +35,30 @@ const CardWrapper = styled('div')<CardWrapper>(
 )
 
 export type CardProps = {
+  /**
+   * Sets whether the entire card is clickable, or not. Should be used in conjunction with `onClick` prop.
+   *
+   * @default true
+   */
   isClickable?: boolean
+  /**
+   * Disables user interaction with the Card component.
+   *
+   * @default false
+   */
   disabled?: boolean
+  /**
+   * The child components that will be rendered.
+   */
   children: React.ReactNode
+  /**
+   * Function that is called when the card is clicked and `isClickable` prop is used.
+   */
   onClick?: React.MouseEventHandler<HTMLDivElement>
 }
 
 export default function Card({
-  isClickable = false,
+  isClickable = true,
   disabled = false,
   children,
   onClick,
@@ -51,7 +67,7 @@ export default function Card({
   return (
     <ThemeProvider theme={defaultTheme}>
       <CardWrapper
-        role={isClickable ? 'button' : 'div'}
+        role={isClickable ? "button" : "div"}
         isClickable={isClickable}
         disabled={disabled}
         onClick={isClickable && !disabled ? onClick : undefined}
--- src/components/Card/stories/CardContent.stories.tsx
+++ src/components/Card/stories/CardContent.stories.tsx
@@ -1,13 +1,39 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Card from '../Card'
-import CardContent from '../CardContent'
+import { Meta, StoryObj } from "@storybook/react"
+import styled from "@emotion/styled"
+import Card from "../Card"
+import CardContent from "../CardContent"
 
 type CardContentStory = StoryObj<typeof CardContent>
 
 const meta: Meta<typeof CardContent> = {
-  title: 'Components/Card/Building Blocks/Card Content',
+  title: "Components/Card/Building Blocks/Card Content",
   component: CardContent,
 }
+
+const CardTitle = styled("h4")`
+  margin: 0px;
+  padding: 0px 0px 24px 0px;
+  font-size: 32px;
+  line-height: 32px;
+  font-family: sans-serif;
+`
+
+const CardSubtitle = styled("p")`
+  margin: 0px;
+  padding: 0px 0px 8px 0px;
+  font-size: 16px;
+  line-height: 24px;
+  font-family: sans-serif;
+  color: rgba(0, 0, 0, 0.7);
+`
+
+const CardParagraph = styled("p")`
+  margin: 0px;
+  padding: 0px 0px 0px 0px;
+  font-size: 16px;
+  line-height: 20px;
+  font-family: sans-serif;
+`
 
 export const Default: CardContentStory = {
   render: (args) => (
@@ -16,7 +42,20 @@ export const Default: CardContentStory = {
     </Card>
   ),
   args: {
-    children: <p style={{ margin: '0px', padding: '0px' }}>Card content</p>,
+    children: (
+      <>
+        <CardContent>
+          <CardSubtitle>Subtitle</CardSubtitle>
+          <CardTitle>Card Title</CardTitle>
+          <CardParagraph style={{ paddingBottom: "12px" }}>
+            Neobrutalism is an aesthetic characterized by high contrast
+            elements, bright colors, and bold shapes. It is often used to make a
+            statement, as it is meant to be eye-catching and stand out to the
+            viewer.
+          </CardParagraph>
+        </CardContent>
+      </>
+    ),
   },
 }
 
