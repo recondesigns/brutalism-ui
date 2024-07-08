176c2e8783b7a4e66ca3842d57951105056a1a84
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-07-08 17:44:01 -0500

    docs(Button): added new props to Button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -9,13 +9,74 @@ const clickFunc = (msg: string) => alert(`${msg} was clicked.`)
 const meta: Meta<typeof Button> = {
   title: 'Form elements/Button',
   component: Button,
+  argTypes: {
+    buttonType: {
+      options: ['block', 'rounded'],
+      control: { type: 'inline-radio' },
+    },
+    size: {
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+      control: { type: 'inline-radio' },
+    },
+  },
 }
 
 export const Default: ButtonStory = {
   render: (args) => <Button {...args} />,
   args: {
+    buttonType: 'block',
+    size: 'md',
+    isFullWidth: false,
+    disabled: false,
     children: 'Button',
     onClick: () => clickFunc('Button was clicked.'),
+  },
+  parameters: {
+    controls: {
+      exclude: ['leftIcon', 'className', 'onClick'],
+    },
+  },
+}
+
+export const ButtonType = {
+  ...Default,
+  args: {
+    buttonType: 'rounded',
+    children: 'Button',
+    onClick: () => clickFunc('Full width button was clicked.'),
+  },
+  parameters: {
+    controls: {
+      exclude: [
+        'disabled',
+        'isFullWidth',
+        'leftIcon',
+        'className',
+        'onClick',
+        'size',
+      ],
+    },
+  },
+}
+
+export const Size = {
+  ...Default,
+  args: {
+    size: 'xl',
+    children: 'Button',
+    onClick: () => clickFunc('Full width button was clicked.'),
+  },
+  parameters: {
+    controls: {
+      exclude: [
+        'disabled',
+        'leftIcon',
+        'className',
+        'onClick',
+        'isFullWidth',
+        'buttonType',
+      ],
+    },
   },
 }
 
@@ -26,6 +87,18 @@ export const FullWidth = {
     isFullWidth: true,
     onClick: () => clickFunc('Full width button was clicked.'),
   },
+  parameters: {
+    controls: {
+      exclude: [
+        'disabled',
+        'leftIcon',
+        'className',
+        'onClick',
+        'size',
+        'buttonType',
+      ],
+    },
+  },
 }
 
 export const LeftIcon = {
@@ -35,6 +108,18 @@ export const LeftIcon = {
     leftIcon: <CloseIcon />,
     onClick: () => clickFunc('Left icon button was clicked.'),
   },
+  parameters: {
+    controls: {
+      exclude: [
+        'isFullWidth',
+        'disabled',
+        'className',
+        'onClick',
+        'size',
+        'buttonType',
+      ],
+    },
+  },
 }
 
 export const Disabled = {
@@ -43,6 +128,19 @@ export const Disabled = {
     children: 'Button',
     disabled: true,
     onClick: () => clickFunc('Disabled button was clicked.'),
+  },
+  parameters: {
+    controls: {
+      exclude: [
+        'buttonType',
+        'leftIcon',
+        'className',
+        'onClick',
+        'size',
+        'buttonType',
+        'isFullWidth',
+      ],
+    },
   },
 }
 
