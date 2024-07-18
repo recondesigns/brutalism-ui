a516b9745cbfbf5735b1eb873071a93e0d462998
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-07-18 02:10:17 -0500

    docs(CardImage): added Demo story and updated docs

--- src/components/Card/stories/CardImage.stories.tsx
+++ src/components/Card/stories/CardImage.stories.tsx
@@ -6,11 +6,42 @@ import CardImage from '../CardImage'
 const cardImage =
   'https://images.unsplash.com/photo-1720206811364-684e8f8e803f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
-type CardImageStory = StoryObj<typeof CardImage>;
+type CardImageStory = StoryObj<typeof CardImage>
 
 const meta: Meta<typeof CardImage> = {
   title: 'UI Components/Card/Building Blocks/Card Image',
   component: CardImage,
+}
+
+export const Demo: CardImageStory = {
+  render: (args) => (
+    <Card>
+      <CardImage {...args} />
+    </Card>
+  ),
+  args: {
+    src: cardImage,
+    alt: 'Card image',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <div
+          style={{
+            maxWidth: '360px',
+            minWidth: '360px',
+          }}
+        >
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
 }
 
 export const Default: CardImageStory = {
@@ -23,6 +54,25 @@ export const Default: CardImageStory = {
     src: cardImage,
     alt: 'Card image',
   },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <div
+          style={{
+            maxWidth: '360px',
+            minWidth: '360px',
+          }}
+        >
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
 }
 
 export default meta
