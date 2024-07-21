0783e4d37a6ba63b1669a8017cf40a0e4442afd6
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-07-21 12:05:42 -0500

    refactor: moved prop type exports to index file

--- src/components/Card/index.ts
+++ src/components/Card/index.ts
@@ -1,1 +1,1 @@
-export { default as Card, CardProps } from './Card'
+export { default as Card } from './Card'
--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,1 +1,1 @@
-export { default, InputProps } from './Input'
+export { default } from './Input'
