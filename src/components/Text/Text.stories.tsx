833496e0f3c21636e692443e9b70c1b536c7f199
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-07-21 23:00:09 -0500

    refactor(Text): changes after build

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { Meta, StoryObj } from "@storybook/react"
-import Text from "./Text"
+import React from 'react'
+import { Meta, StoryObj } from '@storybook/react'
+import Text from './Text'
 
 const meta: Meta<typeof Text> = {
-  title: "Foundations/Text",
+  title: 'Foundations/Text',
   component: Text,
 }
 
@@ -12,22 +12,22 @@ type TextStory = StoryObj<typeof Text>
 export const Demo: TextStory = {
   render: (args) => <Text {...args} />,
   args: {
-    variant: "h1",
+    variant: 'h1',
     gutterBottom: false,
-    children: "Text component",
-    asElement: "h5",
+    children: 'Text component',
+    asElement: 'h5',
   },
 }
 
 export const Variant: TextStory = {
   ...Demo,
   args: {
-    variant: "body1",
-    children: "Text component",
+    variant: 'body1',
+    children: 'Text component',
   },
   parameters: {
     controls: {
-      exclude: ["asElement", "gutterBottom"],
+      exclude: ['asElement', 'gutterBottom'],
     },
   },
 }
@@ -35,13 +35,13 @@ export const Variant: TextStory = {
 export const AsElement: TextStory = {
   ...Demo,
   args: {
-    variant: "h6",
-    children: "Text component",
-    asElement: "p",
+    variant: 'h6',
+    children: 'Text component',
+    asElement: 'p',
   },
   parameters: {
     controls: {
-      exclude: ["gutterBottom", "variant"],
+      exclude: ['gutterBottom', 'variant'],
     },
   },
 }
@@ -49,13 +49,13 @@ export const AsElement: TextStory = {
 export const GutterBottom: TextStory = {
   ...Demo,
   args: {
-    variant: "h4",
-    children: "Text component",
+    variant: 'h4',
+    children: 'Text component',
     gutterBottom: true,
   },
   parameters: {
     controls: {
-      exclude: ["asElement", "variant"],
+      exclude: ['asElement', 'variant'],
     },
   },
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,71 +1,71 @@
-import React from "react"
-import styled from "@emotion/styled"
-import { ThemeProvider } from "@emotion/react"
-import { defaultTheme } from "../emotionTheme"
+import React from 'react'
+import styled from '@emotion/styled'
+import { ThemeProvider } from '@emotion/react'
+import { defaultTheme } from '../emotionTheme'
 
 type Variant =
-  | "h1"
-  | "h2"
-  | "h3"
-  | "h4"
-  | "h5"
-  | "h6"
-  | "subtitle1"
-  | "subtitle2"
-  | "body1"
-  | "body2"
-  | "caption"
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'subtitle1'
+  | 'subtitle2'
+  | 'body1'
+  | 'body2'
+  | 'caption'
 
 const variantMap: { [key in Variant]: React.ElementType } = {
-  h1: "h1",
-  h2: "h2",
-  h3: "h3",
-  h4: "h4",
-  h5: "h5",
-  h6: "h6",
-  subtitle1: "p",
-  subtitle2: "p",
-  body1: "p",
-  body2: "p",
-  caption: "span",
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  h4: 'h4',
+  h5: 'h5',
+  h6: 'h6',
+  subtitle1: 'p',
+  subtitle2: 'p',
+  body1: 'p',
+  body2: 'p',
+  caption: 'span',
 }
 
 function setFontSize(textVariant: Variant) {
   switch (textVariant) {
-    case "h1":
-      return "6rem"
-    case "h2":
-      return "3.75rem"
-    case "h3":
-      return "3rem"
-    case "h4":
-      return "2.125rem"
-    case "h5":
-      return "1.5rem"
-    case "h6":
-      return "1.25rem"
-    case "subtitle1":
-      return "1rem"
-    case "subtitle2":
-      return "0.875rem"
-    case "body1":
-      return "1rem"
-    case "body2":
-      return "0.875rem"
-    case "caption":
-      return "00.75rem"
+  case 'h1':
+    return '6rem'
+  case 'h2':
+    return '3.75rem'
+  case 'h3':
+    return '3rem'
+  case 'h4':
+    return '2.125rem'
+  case 'h5':
+    return '1.5rem'
+  case 'h6':
+    return '1.25rem'
+  case 'subtitle1':
+    return '1rem'
+  case 'subtitle2':
+    return '0.875rem'
+  case 'body1':
+    return '1rem'
+  case 'body2':
+    return '0.875rem'
+  case 'caption':
+    return '00.75rem'
   }
 }
 
-const TextRoot = styled("div")<TypographyProps>(
-  ({ theme, variant = "body1" }) => ({
+const TextRoot = styled('div')<TypographyProps>(
+  ({ theme, variant = 'body1' }) => ({
     fontFamily: theme.typography.fontFamily,
     fontWeight: theme.typography.button.fontWeight,
     fontSize: setFontSize(variant),
     color: theme.palette.common.black,
   }),
   ({ gutterBottom }) => ({
-    padding: gutterBottom ? "0 0 0.35em 0" : "0",
+    padding: gutterBottom ? '0 0 0.35em 0' : '0',
   })
 )
 
@@ -76,12 +76,12 @@ type TypographyProps = {
 } & React.HTMLAttributes<HTMLDivElement>
 
 export default function Text({
-  variant = "body1",
+  variant = 'body1',
   asElement,
   gutterBottom = false,
   ...otherProps
 }: TypographyProps) {
-  const Component = asElement || variantMap[variant] || "p"
+  const Component = asElement || variantMap[variant] || 'p'
 
   return (
     <ThemeProvider theme={defaultTheme}>
