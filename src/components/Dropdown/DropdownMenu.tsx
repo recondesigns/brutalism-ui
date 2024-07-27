c6bca96071afab2b13cf1024380d3bd868862643
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-07-27 18:39:00 -0500

    refactor(Dropdown): changed hover theme style form two to three

--- src/components/Dropdown/DropdownMenu.tsx
+++ src/components/Dropdown/DropdownMenu.tsx
@@ -26,7 +26,7 @@ const DropdownMenuContainer = styled('button')<DropdownMenuContainerProps>(
       : `${defaultTheme.elevation.three} ${defaultTheme.elevation.three} 0px 0px ${defaultTheme.palette.common.shadow}`
     const boxShadowHover = theme.palette
       ? `${theme.elevation.two} ${theme.elevation.two} 0px 0px ${theme.palette.common.shadow}`
-      : `${defaultTheme.elevation.three} ${defaultTheme.elevation.three} 0px 0px ${defaultTheme.palette.common.shadow}`
+      : `${defaultTheme.elevation.two} ${defaultTheme.elevation.two} 0px 0px ${defaultTheme.palette.common.shadow}`
     const boxShadowOpen = theme.palette
       ? `${theme.elevation.none} ${theme.elevation.none} 0px 0px none`
       : `${defaultTheme.elevation.three} ${defaultTheme.elevation.three} 0px 0px none`
