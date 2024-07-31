f5cc6a1b53328ccb06b51fc1ad518b571c9d735b
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2024-07-31 07:47:34 -0500

    feat(Dropdown): added transitions styles for hover and active states

--- src/components/Dropdown/DropdownMenu.tsx
+++ src/components/Dropdown/DropdownMenu.tsx
@@ -1,8 +1,8 @@
-import React from "react"
-import styled from "@emotion/styled"
-import Text from "../Text"
-import { ChevronIcon } from "../../assets"
-import { defaultTheme } from "../emotionTheme"
+import React from 'react'
+import styled from '@emotion/styled'
+import Text from '../Text'
+import { ChevronIcon } from '../../assets'
+import { defaultTheme } from '../emotionTheme'
 
 type DropdownMenuContainerProps = {
   hasError?: boolean
@@ -10,24 +10,24 @@ type DropdownMenuContainerProps = {
   isFlyoutOpen?: boolean
 }
 
-const DropdownMenuContainer = styled("button")<DropdownMenuContainerProps>(
+const DropdownMenuContainer = styled('button')<DropdownMenuContainerProps>(
   {
-    position: "relative",
-    margin: "0px 0px 8px 0px",
-    padding: "12px 20px 12px 20px",
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    textAlign: "left",
+    position: 'relative',
+    margin: '0px 0px 8px 0px',
+    padding: '12px 20px 12px 20px',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    textAlign: 'left',
     // transform: 'translate(-3px, -3px)',
     transition:
-      "box-shadow 150ms cubic-bezier(.645, .045, .355, 1), transform 150ms cubic-bezier(.645,.045,.355,1)",
-    "&:hover": {
-      transform: "translate(-2px, -2px)",
+      'box-shadow 150ms cubic-bezier(.645, .045, .355, 1), transform 150ms cubic-bezier(.645,.045,.355,1)',
+    '&:hover': {
+      transform: 'translate(-2px, -2px)',
     },
-    "&:active": {
-      transform: "translate(-0px, -0px)",
+    '&:active': {
+      transform: 'translate(-0px, -0px)',
     },
   },
   ({ theme, isFlyoutOpen, hasError, disabled }) => {
@@ -40,51 +40,51 @@ const DropdownMenuContainer = styled("button")<DropdownMenuContainerProps>(
 
     return {
       transform: !isFlyoutOpen
-        ? "translate(-3px, -3px)"
-        : "translate(0px, 0px)",
+        ? 'translate(-3px, -3px)'
+        : 'translate(0px, 0px)',
 
       background: !isFlyoutOpen
         ? theme.palette
           ? theme.palette.common.white
           : defaultTheme.palette.common.white
         : theme.palette
-        ? theme.palette.primary.light
-        : defaultTheme.palette.primary.light,
+          ? theme.palette.primary.light
+          : defaultTheme.palette.primary.light,
       border: !hasError
         ? theme.palette
           ? `${theme.elevation.two} solid ${theme.palette.common.border}`
           : `${defaultTheme.elevation.two} solid ${defaultTheme.palette.common.border}`
         : theme.palette
-        ? `${theme.elevation.two} solid ${theme.palette.error.main}`
-        : `${defaultTheme.elevation.two} solid ${defaultTheme.palette.error.main}`,
+          ? `${theme.elevation.two} solid ${theme.palette.error.main}`
+          : `${defaultTheme.elevation.two} solid ${defaultTheme.palette.error.main}`,
       borderRadius: theme.palette
         ? theme.shape.borderRadius
         : defaultTheme.shape.borderRadius,
-      boxShadow: isFlyoutOpen ? "none" : boxShadowClosed,
-      opacity: disabled ? "50%" : "initial",
-      cursor: !disabled ? "pointer" : "not-allowed",
-      "&:hover": {
+      boxShadow: isFlyoutOpen ? 'none' : boxShadowClosed,
+      opacity: disabled ? '50%' : 'initial',
+      cursor: !disabled ? 'pointer' : 'not-allowed',
+      '&:hover': {
         transform: !isFlyoutOpen
-          ? "translate(-2px, -2px)"
-          : "translate(0px, 0px)",
+          ? 'translate(-2px, -2px)'
+          : 'translate(0px, 0px)',
         background: !disabled
           ? theme.palette
             ? theme.palette.primary.light
             : defaultTheme.palette.primary.light
           : theme.palette
-          ? theme.palette.common.white
-          : defaultTheme.palette.common.white,
+            ? theme.palette.common.white
+            : defaultTheme.palette.common.white,
         boxShadow: isFlyoutOpen
-          ? "none"
+          ? 'none'
           : !disabled
-          ? boxShadowHover
-          : "3px 3px 0px 0px black",
+            ? boxShadowHover
+            : '3px 3px 0px 0px black',
       },
-      "&:active": {
+      '&:active': {
         transform: !isFlyoutOpen
-          ? "translate(0px, 0px)"
-          : "translate(0px, 0px)",
-        boxShadow: !disabled ? "none" : "3px 3px 0px 0px black",
+          ? 'translate(0px, 0px)'
+          : 'translate(0px, 0px)',
+        boxShadow: !disabled ? 'none' : '3px 3px 0px 0px black',
       },
     }
   }
@@ -94,19 +94,19 @@ type IconContainerProps = {
   isOpen?: boolean
 }
 
-const IconContainer = styled("div")<IconContainerProps>(
+const IconContainer = styled('div')<IconContainerProps>(
   {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   ({ isOpen }) => ({
-    transform: isOpen ? "rotate(180deg)" : "initial",
+    transform: isOpen ? 'rotate(180deg)' : 'initial',
   })
 )
 
 const ButtonText = styled(Text)({
-  width: "100%",
+  width: '100%',
 })
 
 type Option = {
@@ -146,7 +146,7 @@ const DropdownMenu = React.forwardRef<HTMLButtonElement, DropdownMenuProps>(
         onKeyDown={onKeyDown}
       >
         <ButtonText variant="body1">
-          {value ? value.name : "Choose an option..."}
+          {value ? value.name : 'Choose an option...'}
         </ButtonText>
         <IconContainer isOpen={isFlyoutOpen}>
           <ChevronIcon />
@@ -156,6 +156,6 @@ const DropdownMenu = React.forwardRef<HTMLButtonElement, DropdownMenuProps>(
   }
 )
 
-DropdownMenu.displayName = "DropdownMenu"
+DropdownMenu.displayName = 'DropdownMenu'
 
 export default DropdownMenu
