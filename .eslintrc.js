25fe9d46a5d4e47df28f670914f8894639c8a9f4
thestedman <mstedman@rockislandauction.com>
2025-11-02 15:02:46 -0600

    chore(): commented out semicolon eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,8 +33,8 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
+    // quotes: ["error", "single"],
+    // semi: ["error", "never"],
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
     "@typescript-eslint/no-explicit-any": "warn",
