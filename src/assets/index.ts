4aed3905bfc1dae1213201af0d5931b2d078a5ad
thestedman <mstedman@rockislandauction.com>
2025-11-03 01:17:34 -0600

    refactor(Icon): there was a re-build for the icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
-export { default as AlertIcon } from './AlertIcon'
-export { default as ChevronIcon } from './ChevronIcon'
-export { default as CloseIcon } from './CloseIcon'
-export { default as PhoneIcon } from './PhoneIcon'
-export { default as ReadmeLogo } from './ReadmeLogo'
+export { default as AlertIcon } from "./AlertIcon";
+export { default as ChevronIcon } from "./ChevronIcon";
+export { default as CloseIcon } from "./CloseIcon";
+export { default as PhoneIcon } from "./PhoneIcon";
+export { default as ReadmeLogo } from "./ReadmeLogo";
